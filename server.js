9d520d0905f63d63140cb8a80dd584cd4c242190
Calista Bell <cbell62@uncc.edu>
2020-10-28 17:15:51 -0400

    updates

--- server.js
+++ server.js
@@ -1,67 +1,58 @@
-// Budget API
-
-const express = require("express");
-const cors = require("cors");
+//Budget API
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
-const mongoose = require("mongoose");
-const budgetModel = require("./models/budgetSchema");
+const budgetModel = require('./models/budget_schema')
+let url = 'mongodb://localhost:27017/budget';
 
-let url = "mongodb://localhost:27017/budget";
-var bodyParser = require("body-parser");
-
-app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use("/", express.static("public"));
+app.use(cors());
+app.use('/', express.static('public'));
 
-app.get("/hello", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.get("/budget", (req, res) => {
-  mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-          .then(() => {
-            console.log("Connected to the database");
-            budgetModel.find({})
-                        .then((data)=>{
-                          console.log(data);
-                          res.json(data);
-                          mongoose.connection.close();
-                        })
-                        .catch((connectionError)=>{
-                          console.log(connectionError);
-                        })  
-          })
-          .catch((connectionError) => {
-            console.log(connectionError);
-          })
-});
-
-app.put("/add", (req, res) => {
-  console.log(req.body);
-  mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-          .then(()=>{
-            console.log("Connected to the database");
-            let addBudget = new budgetModel({
-              title: req.body.title,
-              value: req.body.value,
-              color: req.body.color,
+app.get('/budget', (req, res) => {
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        budgetModel.find({})
+            .then((data) => {
+                console.log(data)
+                res.json(data);
+                mongoose.connection.close();
+            })
+            .catch((connectionError) => {
+                console.log(connectionError);
             });
-            budgetModel.insertMany(addBudget)
-                        .then((data)=> {
-                          console.log(data);
-                          res.json(data);
-                          mongoose.connection.close();
-                        })
-                        .catch((connectionError)=>{
-                          console.log(connectionError);
-                        })  
-          })
-          .catch((connectionError) => {
-            console.log(connectionError);
-          })
+    })
+    .catch((connectionError) => {
+        console.log(connectionError)
+    });
+});
+
+app.put('/addBudget', (req, res) => {
+    mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+         newBudget = {
+             title: req.body.title,
+             value: req.body.value,
+             color: req.body.color,
+         }
+        budgetModel.insertMany(newBudget)
+            .then((data) => {
+            res.json(data);
+            mongoose.connection.close();
+        })
+        .catch((connectionError) => {
+            console.log(connectionError)
+        });
+    })
+        .catch((connectionError) => {
+            console.log(connectionError)
+    });
 });
 
 app.listen(port, () => {
-  console.log(`API served at http://localhost:${port}`);
+    console.log(`API served at http://localhost:${port}`);
 });
