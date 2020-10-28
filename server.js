8ee4f7ab54619ec2e9843b2def27747fb55a546b
Calista Bell <cbell62@uncc.edu>
2020-10-28 17:31:08 -0400

    updates

--- server.js
+++ server.js
@@ -1,58 +1,58 @@
-//Budget API
 const express = require('express');
-const cors = require('cors');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const budgetItemsModel = require('./models/budget_items_schema');
+
 const app = express();
 const port = 3000;
-const budgetModel = require('./models/budget_schema')
-let url = 'mongodb://localhost:27017/budget';
+const url = 'mongodb://localhost:27017/budget';
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cors());
 app.use('/', express.static('public'));
+app.use(express.json());
 
-app.get('/budget', (req, res) => {
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => {
-        budgetModel.find({})
-            .then((data) => {
-                console.log(data)
-                res.json(data);
-                mongoose.connection.close();
-            })
-            .catch((connectionError) => {
-                console.log(connectionError);
-            });
-    })
-    .catch((connectionError) => {
-        console.log(connectionError)
-    });
+app.get('/hello', (req, res) => {
+    res.send('Hello World!');
 });
 
-app.put('/addBudget', (req, res) => {
+app.get('/budget', (req, res) => {
     mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-         newBudget = {
-             title: req.body.title,
-             value: req.body.value,
-             color: req.body.color,
-         }
-        budgetModel.insertMany(newBudget)
-            .then((data) => {
-            res.json(data);
-            mongoose.connection.close();
+        .then(() => {
+            budgetItemsModel.find({})
+                .then((data) => {
+                    res.json(data);
+                    mongoose.connection.close();
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
         })
-        .catch((connectionError) => {
-            console.log(connectionError)
+        .catch((error) => {
+            console.log(error);
         });
-    })
-        .catch((connectionError) => {
-            console.log(connectionError)
-    });
+});
+
+app.post('/budget', (req, res) => {
+    mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => {
+            var budgetItem = new budgetItemsModel({
+                title: req.body.title,
+                value: req.body.value,
+                color: req.body.color
+            });
+
+            budgetItemsModel.insertMany(budgetItem)
+                .then((data) => {
+                    res.json(data);
+                    mongoose.connection.close();
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 });
 
 app.listen(port, () => {
-    console.log(`API served at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
